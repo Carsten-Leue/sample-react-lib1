013219536fa86d05b19ed49b6d5afc4c8462ce21
Dr. Carsten Leue <carsten.leue@gmx.net>
2019-11-16 21:53:46 +0100

    initial checkin

---
+++ src/lib/components/test-1/test.component1.tsx
@@ -0,0 +1,4 @@
+import { FC } from "react";
+import * as React from 'react';
+
+export const TestComponent1: FC = () => <div className={'test-component-1'}>TestComponent1</div>;
---
+++ src/lib/components/test-2/test.component2.tsx
@@ -0,0 +1,4 @@
+import { FC } from "react";
+import * as React from 'react';
+
+export const TestComponent2: FC = () => <div className={'test-component-2'}>TestComponent2</div>;
